c65b85ef045924b192ff6783325dc64abbd3d178
Bunrawat Charoenyuennan <158060569+Pomaccel@users.noreply.github.com>
2024-09-28 03:53:00 +0000

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # Your branch is up to date with 'origin/main'. # # Changes to be committed: #	modified:   streamlit_app.py #

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🎲 Bunrawat Chat bot  🃏 ")
+if user_input := st.text_input("You: ", placeholder="Type your message here..."):
+    st.write(f"User Input: {user_input}")
